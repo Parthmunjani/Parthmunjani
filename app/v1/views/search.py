2b4fe30fa4462cb1db86a06ace5b406d9b29c854
Parthmunjani <parth.munjani@sculptsoft.com>
2023-06-12 17:24:54 +0530

    migration

--- app/v1/views/search.py
+++ app/v1/views/search.py
@@ -7,6 +7,30 @@ class ProductSearch(Resource):
         try:
             search_term = request.args.get('search_term')
             sort_order = request.args.get('sort_order')
+            page = int(request.args.get('page', 1)) 
+            
+            per_page = int(request.args.get('per_page', 10)) 
+            query = ProductModel.query.filter(ProductModel.name.like('%' + search_term + '%'))
+            if sort_order == 'low_to_high':
+                query = query.order_by(ProductModel.price.asc())
+            elif sort_order == 'high_to_low':
+                query = query.order_by(ProductModel.price.desc())
+
+            offset = (page - 1) * per_page # for product show in page 
+            paginated_query = query.offset(offset).limit(per_page)
+            results = paginated_query.all()
+            if not results:
+                return {"status": False, "detail": "Product Not Found"}
+            serialized_results = [product.to_json() for product in results]
+            return {"status": True, "details": serialized_results}, 200
+        except Exception as e:
+            return {"status": False, "detail": str(e)}, 400
+        
+"""class ProductSearch(Resource):
+    def get(self):
+        try:
+            search_term = request.args.get('search_term')
+            sort_order = request.args.get('sort_order')
             page = int(request.args.get('page', 1))
             #print(sort_order)
             query = ProductModel.query.filter(ProductModel.name.like('%'+search_term+'%'))
@@ -24,5 +48,5 @@ class ProductSearch(Resource):
             serialized_results = [product.to_json() for product in results]
             return {"status": True, "details": serialized_results}, 200
         except Exception as e:
-            return {"status": False, "detail": str(e)}, 400
+            return {"status": False, "detail": str(e)}, 400"""
 
